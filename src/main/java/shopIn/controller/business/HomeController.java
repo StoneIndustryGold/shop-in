f60c9897c461add6fac81c226668a7bec99a2edf
ShiQinJin <1786001656@qq.com>
2018-07-19 20:06:41 +0800

    让首页显示信息及图片--

--- src/main/java/shopIn/controller/business/HomeController.java
+++ src/main/java/shopIn/controller/business/HomeController.java
@@ -1,13 +1,32 @@
 package shopIn.controller.business;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import shopIn.pojo.Cellpones;
+import shopIn.sevice.CellponesService;
 @Controller
 public class HomeController {
+		
+	private CellponesService cellponesService;
 	
+	@Autowired
+	public HomeController(CellponesService cellponesService) {
+		this.cellponesService = cellponesService;
+	}
+		
+
 	 @RequestMapping(method = RequestMethod.GET, value = "/")
-	    public String home() {
+	    public String home(Model model) {
+		 List<Cellpones> cellpones=cellponesService.findAll();
+		model.addAttribute("cellpones", cellpones);
 	        return "index";
 	    }
+
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,7 +1,21 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
   <%@ taglib prefix="t" tagdir="/WEB-INF/tags" %>  
-
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+ <c:set var="contextPath" value="${pageContext.request.contextPath}"></c:set>
 <t:layout>
 	<h2>欢迎来到首页 http://localhost:8080/shop-in/</h2>
+		<div>
+		<ul>
+			<c:forEach items="${cellpones }" var="cellpones">
+			
+				<li>
+				<a href="${contextPath }/cellpones/${cellpones.id }/details">
+				 <img  src="${contextPath}/assets/images/cellpone/${cellpones.images }"
+				  width="100" height="100" align="middle"></a></li>
+				<li><a href="${contextPath }/cellpones/${cellpones.id }/details">${cellpones.brand }</a>				
+				</li>																
+			</c:forEach>		
+		</ul>
+	</div>
 </t:layout>
