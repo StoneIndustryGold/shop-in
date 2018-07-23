c5eaacdf73688b9a644e1d47430e1e747e67dab8
ShiQinJin <1786001656@qq.com>
2018-07-23 10:58:03 +0800

    增加了数据库的表购物车

--- scripts/db/init-db.sql
+++ scripts/db/init-db.sql
@@ -10,7 +10,7 @@
 --price--价格
 --images --图片
 
-create table cellpones(--手机表
+create table cellpones(----------------------------------------------手机表
        id integer primary key,
        brand varchar(64) not null,--牌子
        model varchar(64) not null,--型号
@@ -29,4 +29,55 @@ create sequence cellpones_seq
        START WITH 1    -- 从1开始计数  
        NOMAXVALUE      -- 不设置最大值  
        NOCYCLE -- 一直累加，不循环  
+       
+ =------------- ----------------------------------------用户表
+ 		--id
+ 用户名字--username
+ 密码------password
+ 性别--sex
+ 年龄--age
+ 邮箱--GMail
+ 是否启用--enabled
+ 图像---images
+ 
+ create table users(-----用户表
+        id integer primary key,
+        username varchar(64) not null,
+        password varchar(128) not null,
+        sex varchar(64),
+        age_id integer ,
+        gmail varchar(128),
+        enabled varchar2(1),
+        images varchar(2000),
+        LAST_LOGIN_TIME timestamp
+ )
+ 
+ 
+ create sequence users_seq
+       INCREMENT BY 1  -- 每次加几个  
+       START WITH 1    -- 从1开始计数  
+       NOMAXVALUE      -- 不设置最大值  
+       NOCYCLE   -- 一直累加，不循环  
+       
+       
+ ------------------------------------------------------ --购物车
+--- 用户id
+--手机id
+--数量 
+create table carts(
+       id integer primary key,
+       user_id integer not null,
+       cellpone_id integer not null,
+       count integer     
+  ) 
+
+ create sequence carts_seq
+        INCREMENT BY 1  -- 每次加几个  
+       START WITH 1    -- 从1开始计数  
+       NOMAXVALUE      -- 不设置最大值  
+       NOCYCLE
+       
+ alter table carts add foreign key (user_id) references users(id)
+ alter table carts add foreign key (cellpone_id)references cellpones(id) 
+       
                 
